2acd4e9184c0c2791f32cfa627d030b44dc8bc58
Bright Amoateng <bright.amoateng150@gmail.com>
2022-04-17 18:48:11 -0400

    Use ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,10 @@ module.exports = {
     output: {
         filename: "build.js",
         path: path.resolve(__dirname, "public"),
-  },
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
 };
